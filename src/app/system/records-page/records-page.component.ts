96bfd9b4f621bdef923b88a5da9cc27e5329edcf
Anatoliy Geraschenko <ageraschenko@highedge.com>
2019-03-29 16:36:55 +0200

    add system module with page

---
+++ src/app/system/records-page/records-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sts-records-page',
+  templateUrl: './records-page.component.html',
+  styleUrls: ['./records-page.component.scss']
+})
+export class RecordsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
